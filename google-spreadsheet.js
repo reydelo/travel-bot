f3c1b5fedf0630a205a17469fe4b2ebd29d07a00
Reyna D <reyna.deloge@gmail.com>
2020-06-28 20:25:17 +0200

    use consistent property names

--- google-spreadsheet.js
+++ google-spreadsheet.js
@@ -28,44 +28,24 @@ async function getTrainInfoForUser(email) {
 }
 
 async function updateTrainInfo(formSubmission) {
-  const rowData = {
-    email: formSubmission.email,
-    homestation: formSubmission.home_station,
-    destinationstation: formSubmission.destination_station,
-    homedeparturetime: formSubmission.home_departure_time,
-    destinationdeparturetime: formSubmission.destination_departure_time,
-    bahncardtype: formSubmission.bahncard_type,
-    bahncardnumber: formSubmission.bahncard_number
-  };
-
   await setAuth();
   const [_, trainInfoSheet] = await getWorksheets();
   const row = await findMatchingRow(trainInfoSheet, formSubmission.email);
 
   if (row) {
-    for (let prop in rowData) {
-      row[prop] = rowData[prop];
+    for (let prop in formSubmission) {
+      row[prop] = formSubmission[prop];
     }
     row.save();
   } else {
-    trainInfoSheet.addRow(rowData);
+    trainInfoSheet.addRow(formSubmission);
   }
 }
 
 async function submitTravelRequest(formSubmission) {
-  const rowData = {
-    timestamp: Date.now(),
-    email: formSubmission.email,
-    traveltype: formSubmission.travel_type,
-    outwarddate: formSubmission.outward_date,
-    returndate: formSubmission.return_date,
-    travelreason: formSubmission.travel_reason,
-    travelmessage: formSubmission.travel_message
-  };
-
   await setAuth();
   const [travelRequestSheet] = await getWorksheets();
-  const newRow = await travelRequestSheet.addRow(rowData);
+  const newRow = await travelRequestSheet.addRow({ timestamp: Date.now(), ...formSubmission });
 
   newRow.save();
 }
